2143e1339543f1795b8834b6fc52c7e5f0fb636d
carlos <carlossilvameloo@gmail.com>
2021-02-28 00:33:54 -0300

    update

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -3,10 +3,10 @@ import sendMsg from '../../services/slackSender';
 export default function handler(req, res) {
     
     const { method, params } = req;
-    console.log(req.body.challenge)
+    console.log(req.body.event.text)
     if(method === 'POST'){
-        sendMsg(JSON.stringify(req.body)).catch(err => console.log(err));
-        res.status(200).json(req.body)
+        sendMsg(JSON.stringify(req.body.event.text)).catch(err => console.log(err));
+        res.status(200).json({})
     }
     
 }
