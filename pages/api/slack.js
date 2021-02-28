8b14f04509cc480a2fc21b6e95b692c2f459fef5
carlos <carlossilvameloo@gmail.com>
2021-02-27 22:50:58 -0300

    update

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -1,11 +1,8 @@
 export default function handler(req, res) {
     // sendMsg().catch(err => console.log(err));
     const { method, params } = req;
-    console.log(req.body.challenge)
     if(method === 'POST'){
-        res.setHeader('content-type', 'text/plain');
-        res.status(200);
-        res.send(req.body.challenge);
+        res.status(200).json(req.body)
     }
     
 }
