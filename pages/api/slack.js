46ac8de6586fd0b2619c4b5da039d7287e984a81
carlos <carlossilvameloo@gmail.com>
2021-02-28 00:36:58 -0300

    toma

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
     const { method, params } = req;
     console.log(req.body.event.text)
     if(method === 'POST'){
-        sendMsg(JSON.stringify(req.body.event.text)).catch(err => console.log(err));
+        sendMsg(req.body.event.text).catch(err => console.log(err));
         res.status(200).json({})
     }
     
