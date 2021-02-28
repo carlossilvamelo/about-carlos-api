cfc5427425091c071f3cd0cb288dd780163855d4
carlos <carlossilvameloo@gmail.com>
2021-02-27 23:51:16 -0300

    show

--- pages/api/live.js
+++ pages/api/live.js
@@ -1,7 +1,8 @@
-import sendMsg from '../../services/slackSender';
-
+// import sendMsg from '../../services/slackSender';
+const SLACK_TOKEN = process.env.TOKEN_SLACK;
+const URL_SLACK = process.env.URL_SLACK;
 export default function handler(req, res) {
 
-  sendMsg().catch(err => console.log(err));
-  res.status(200).json({ name: 'John Doe' })
+  // sendMsg().catch(err => console.log(err));
+  res.status(200).json({ tk: SLACK_TOKEN, url:URL_SLACK })
 }
